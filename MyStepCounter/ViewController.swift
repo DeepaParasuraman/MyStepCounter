74efb10f3c12f456f92c9f771d66d789050f1177
deepaMacno <deepamacno@gmail.com>
2023-08-05 21:54:40 +0400

    Initial Commit

---
+++ MyStepCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStepCounter
+//
+//  Created by MACNO on 05/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
