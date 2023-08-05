f850fdb21e299414d0e25295eb5c6799f10a48d5
deepaMacno <deepamacno@gmail.com>
2023-08-06 00:38:45 +0400

    first commit

---
+++ MyStepCounter/Files/Definition.swift
@@ -0,0 +1,8 @@
+//
+//  Definition.swift
+//  MyStepCounter
+//
+//  Created by MACNO on 05/08/2023.
+//
+
+import Foundation
---
+++ MyStepCounter/Files/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  MyStepCounter
+//
+//  Created by MACNO on 05/08/2023.
+//
+
+import Foundation
